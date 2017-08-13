484852bf9eae77e38257d1041b8a1b63cbbe49b5
Michal Pyrka <michal.pyrka@droidsonroids.pl>
2017-08-13 17:47:31 +0200

    Initial Commit

---
+++ ShadowCircleRenderer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShadowCircleRenderer
+//
+//  Created by Michal Pyrka on 13/08/2017.
+//  Copyright © 2017 Michal Pyrka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
